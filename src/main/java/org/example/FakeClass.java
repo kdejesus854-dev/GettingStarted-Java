d4584e7f126290b1e4bfe99a4243f9d7a2532f99
kdejesus854-dev <kdejesus854@gmail.com>
2026-09-13 15:30:58 -0400

    Initial commit

---
+++ src/main/java/org/example/FakeClass.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class FakeClass {
+    public int age;
+    public String firstName;
+    public  String lastName;
+
+    public FakeClass(int age, String firstName, String lastName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge(){
+    return this.age;
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] int_array = {1, 2, 3, 4, 5, 6};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < int_array.length; i++) {
+            System.out.println(int_array[i]);
+        }
+        ArrayList<Integer> dyn_list = new ArrayList<>();;
+        dyn_list.add(1);
+        dyn_list.add(2);
+        dyn_list.add(7);
+        dyn_list.add(42);
+        dyn_list.add(67);
+
+        for(int i : dyn_list) {
+            System.out.println(i);
+        }
+        for (Integer integer : dyn_list) {
+            System.out.println(integer);
         }
     }
+
 }
